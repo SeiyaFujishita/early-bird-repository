6f02b6df03b83abacf807a8253ba0d64bf0d8390
daichinagano <d.nagano1002@gmail.com>
2022-12-26 10:03:09 +0900

    🐛 add tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // tailwindを適用したいファイル群を指定
   content: [
-    "./app.vue",
+    "./app.vue", // <= ドキュメントからさらに追加
     "./components/**/*.{vue,js}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
   ],
   theme: {
     extend: {},
