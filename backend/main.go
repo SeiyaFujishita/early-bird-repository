bb1e602a05e0c3ec427e8d485b52b7e59be2fd41
daichinagano <d.nagano1002@gmail.com>
2023-01-06 18:26:21 +0900

    ✨ init commit for server

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	// 環境変数読み込み
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Failed to connect env: %v", err)
+	}
+
+	// DB初期化処理
+	// gormHandler := infra.NewGormHandler()
+
+	// リポジトリ初期化処理
+	// repos := domainrepo.Repositories{}
+
+	router := gin.Default()
+
+	setCors(router)
+
+	// usecase初期化処理
+	// server.NewApiServer(ctx, router, repos)
+
+	router.Run()
+	log.Print("server start!")
+}
+
+func setCors(r *gin.Engine) {
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 }
