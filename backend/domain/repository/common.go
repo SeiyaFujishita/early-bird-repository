8cbf487798961bb49c9c2ffda4e9e12aa40e1059
SeiyaFujishita <hakugen07@gmail.com>
2023-01-16 18:59:28 +0900

    ⚡️ resolve conflict

--- backend/domain/repository/common.go
+++ backend/domain/repository/common.go
@@ -3,4 +3,5 @@ package repository
 type Repositories struct {
 	TaskRepository   TaskRepository
 	ActiveRepository ActiveRepository
+	UserRepository UserRepository
 }
--- backend/server/server.go
+++ backend/server/server.go
@@ -10,7 +10,9 @@ import (
 func NewApiServer(router *gin.Engine, repos repository.Repositories) {
 	taskUsecase := usecase.NewTaskUsecase(repos.TaskRepository)
 	activeUsecase := usecase.NewActiveUsecase(repos.ActiveRepository)
+	userUsecase := usecase.NewUserUsecase(repos.UserRepository)
 
 	router.GET("/tasks", func(c *gin.Context) { taskUsecase.ListTasks(c) })
 	router.POST("/actives", func(c *gin.Context) { activeUsecase.CreateActive(c) })
+	router.POST("/user", func(c *gin.Context) { userUsecase.CreateUser(c) })
 }
