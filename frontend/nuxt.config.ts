0debc53ce134d8b4e5fd57a9ad7fd6574b416b3b
SeiyaFujishita <hakugen07@gmail.com>
2023-06-02 10:03:32 +0900

    🚧  jsからcjsに変更してみる

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
     head: {
       script: [
         {
-          src: "https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js",
+          src: "https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.umd.min.js",
         },
       ],
     },
